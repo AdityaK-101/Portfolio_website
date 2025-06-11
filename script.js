7d8a26ba8cba6ecdd12c18b45ae3eacebd3f2069
AdityaK-101 <kulkarni.aditya.m@gmail.com>
2025-06-11 10:03:26 +0530

    update

--- script.js
+++ script.js
@@ -28,14 +28,6 @@ function setTheme(mode){
 		document.getElementById('theme-style').href = 'blue.css'
 	}
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
 	if(mode == 'dark'){
 		document.getElementById('theme-style').href = 'dark.css'
 	}
